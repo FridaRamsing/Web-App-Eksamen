b52b73b0d2a08a9fc8960f8e3b4529117bd9c172
Frida Ramsing <eaa25flbr@students.eaaa.dk>
2026-05-29 10:35:52 +0200

    Add welcome page and auth routes - yay

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 
 import { Routes, Route } from "react-router";
+import WelcomePage from "./pages/WelcomePage";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
@@ -9,7 +10,9 @@ import NotFoundPage from "./pages/NotFoundPage";
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<WelcomePage />} />
+      <Route path="/welcome" element={<WelcomePage />} />
+      <Route path="/Home" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignUpPage />} />
       <Route path="*" element={<NotFoundPage />} />
---
+++ src/pages/WelcomePage.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router";
+import welcomePageImage from "../../Images/Welcome page.svg";
+
+export default function WelcomePage() {
+  return (
+    <main className="auth-page">
+      <section className="auth-shell">
+        <img
+          className="welcome-image"
+          src={welcomePageImage}
+          alt="Welcome illustration for Fjera"
+        />
+
+        <div className="auth-content">
+          <div className="auth-actions">
+            <Link to="/login" className="auth-button">
+              Login
+            </Link>
+
+            <Link to="/signup" className="auth-button">
+              Sign up
+            </Link>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
